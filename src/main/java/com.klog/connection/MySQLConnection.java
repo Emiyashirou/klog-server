7522500b03e21ca8994376e42c8400a4a50a249b
lei.li <lei.li@slalom.com>
2018-09-12 16:34:27 -0500

    Secret

--- src/main/java/com.klog/connection/MySQLConnection.java
+++ src/main/java/com.klog/connection/MySQLConnection.java
@@ -1,8 +1,15 @@
 package com.klog.connection;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
+import com.google.gson.Gson;
+import com.klog.exception.InternalServerErrorException;
+import com.klog.model.aws.DBSecret;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MySQLConnection {
 
@@ -13,19 +20,58 @@ public class MySQLConnection {
      */
     static public Connection connect() {
 
-        String url = "";
-        String user = "";
-        String password = "";
-
-        Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            DBSecret dbSecret = getSecret();
+            if (dbSecret == null) {
+                throw new InternalServerErrorException("Get secrets failed");
+            } else {
+                String url = dbSecret.getHost();
+                String user = dbSecret.getUsername();
+                String password = dbSecret.getPassword();
+
+                return DriverManager.getConnection(url, user, password);
+            }
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+    }
+
+    static private DBSecret getSecret() {
+
+        String secretName = "Mysql";
+        String endpoint = "secretsmanager.us-east-2.amazonaws.com";
+        String region = "us-east-2";
+
+        AwsClientBuilder.EndpointConfiguration config = new AwsClientBuilder.EndpointConfiguration(endpoint, region);
+        AWSSecretsManagerClientBuilder clientBuilder = AWSSecretsManagerClientBuilder.standard();
+        clientBuilder.setEndpointConfiguration(config);
+        AWSSecretsManager client = clientBuilder.build();
+
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = null;
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+
+        } catch (ResourceNotFoundException e) {
+            System.out.println("The requested secret " + secretName + " was not found");
+        } catch (InvalidRequestException e) {
+            System.out.println("The request was invalid due to: " + e.getMessage());
+        } catch (InvalidParameterException e) {
+            System.out.println("The request had invalid params: " + e.getMessage());
         }
 
-        return conn;
+        if (getSecretValueResult == null) {
+            return null;
+        }
+
+        if (getSecretValueResult.getSecretString() != null) {
+            Gson gson = new Gson();
+            return gson.fromJson(getSecretValueResult.getSecretString(), DBSecret.class);
+        } else {
+            return null;
+        }
     }
+
 }
 
